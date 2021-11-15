16581d914be2d12d94f9dcf5adbceb7761b8baa5
subratsahu1314 <subratsahu1314@gmail.com>
2021-11-15 22:58:25 +0530

    Adding 4th Question of Java11 Assignment

---
+++ Question4.java
@@ -0,0 +1,38 @@
+package Java11;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+	      var path="C:\\Users\\subrat\\Desktop\\Java11Assignments_StudentList.txt";
+	      //C:\\Users\\subrat\\Desktop\\Java11Assignments_StudentList.txt
+	      try {
+	    	  String data=Files.readString(Path.of(path));
+	    	  System.out.println(data);
+	    	  List<String>str=data.lines()
+	    			  .map(S->S.trim())
+	    			  .filter(S->!S.isEmpty())
+	    			  .collect(Collectors.toList());
+	    	  for(String string : str) {
+	    		  System.out.println(string);
+	    	  }
+	    	 
+	    	   System.out.println("The number of students is:"+str.size());
+	      
+	      }
+	      
+	    	  catch(IOException e) {
+	    		  e.printStackTrace();
+	    	  }
+	      
+	      
+	      
+	      
+	    	 
+}
+}
